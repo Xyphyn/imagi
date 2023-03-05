83db2c5bb1bca21de9e8b04425057a81009d7ba9
Xylight <james@xylight.us>
2023-03-05 11:33:54 -0800

    v2 init

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
-
-process.env = {...process.env, ...loadEnv('dev', process.cwd())}
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	ssr: {
-		// Freak you, svelte-fa!
-		noExternal: ['@fortawesome/free-solid-svg-icons']
-	}
+	plugins: [sveltekit()]
 });
