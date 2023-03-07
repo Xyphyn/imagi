fd32f5ac22e452f72e1c13bb737e63ce83f2c1ee
Xylight <james@xylight.us>
2023-03-07 15:30:41 -0800

    v2: Add account settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import Button from '$lib/Button.svelte'
     import Colored from '$lib/misc/Colored.svelte'
+    import { currentUser, pb } from '$lib/pocketbase'
     import {
         Disclosure,
         DisclosureButton,
@@ -8,28 +10,36 @@
     } from '@rgossiaux/svelte-headlessui'
     import { Icon, ChevronRight } from 'svelte-hero-icons'
     import { userSettings } from '../../stores'
+
+    interface AccountSettings {
+        email: string
+    }
+
+    let accountSettings: AccountSettings = {
+        email: $currentUser?.email ?? '',
+    }
 </script>
 
-<Disclosure
+<div
     class="flex flex-col items-center justify-center max-w-xl mx-auto dark:bg-slate-800 bg-white p-4 rounded-lg w-full shadow-lg gap-4"
 >
-    <Colored
-        ><h1 class="font-bold text-4xl self-center mx-auto">
-            User Settings
-        </h1></Colored
-    >
-    <DisclosureButton
-        class="flex flex-row self-start justify-between w-full gap-4 p-4 bg-slate-50 dark:bg-slate-700 rounded-lg"
-        let:open
-        ><span>General</span>
-        <Icon
-            src={ChevronRight}
-            size="20"
-            class={`transition-transform ${open ? 'rotate-90' : ''}`}
-        /></DisclosureButton
-    >
-    <DisclosurePanel>
-        <div
+    <Disclosure class="w-full">
+        <Colored
+            ><h1 class="font-bold text-4xl self-center mx-auto">
+                User Settings
+            </h1></Colored
+        >
+        <DisclosureButton
+            class="flex flex-row self-start justify-between w-full gap-4 p-4 bg-slate-50 dark:bg-slate-700 rounded-lg"
+            let:open={general}
+            ><span>General</span>
+            <Icon
+                src={ChevronRight}
+                size="20"
+                class={`transition-transform ${general ? 'rotate-90' : ''}`}
+            /></DisclosureButton
+        >
+        <DisclosurePanel
             class="flex flex-row justify-between w-full items-center p-4 my-2 dark:bg-slate-700 bg-slate-50 rounded-lg"
         >
             <span class="flex flex-col w-[80%]"
@@ -55,6 +65,40 @@
                   pointer-events-none inline-block h-4 w-4 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
                 /></Switch
             >
-        </div>
-    </DisclosurePanel>
-</Disclosure>
+        </DisclosurePanel>
+    </Disclosure>
+    <Disclosure class="w-full">
+        {#if $currentUser}
+            <DisclosureButton
+                class="flex flex-row self-start justify-between w-full gap-4 p-4 bg-slate-50 dark:bg-slate-700 rounded-lg"
+                let:open={account}
+                ><span>Account</span>
+                <Icon
+                    src={ChevronRight}
+                    size="20"
+                    class={`transition-transform ${account ? 'rotate-90' : ''}`}
+                /></DisclosureButton
+            >
+            <DisclosurePanel
+                class="flex flex-row justify-between w-full items-center p-4 my-2 dark:bg-slate-700 bg-slate-50 rounded-lg"
+            >
+                <span>Email</span>
+                <div class="flex flex-row gap-2">
+                    <input
+                        type="text"
+                        placeholder="New Email"
+                        bind:value={accountSettings.email}
+                    />
+                    <Button
+                        major={true}
+                        onclick={() => {
+                            pb.collection('users').requestEmailChange(
+                                accountSettings.email
+                            )
+                        }}>Change</Button
+                    >
+                </div>
+            </DisclosurePanel>
+        {/if}
+    </Disclosure>
+</div>
