8fb6f5484c47f25decffb7334847c3739004b2ab
Xylight <james@xylight.us>
2023-03-06 17:24:18 -0800

    v2: fix serverless function crash

--- src/routes/post/[id]/+page.server.ts
+++ src/routes/post/[id]/+page.server.ts
@@ -1,14 +1,14 @@
 import { pb } from '$lib/pocketbase'
 import { error } from '@sveltejs/kit'
+import eventsource from 'eventsource'
 
 // @ts-ignore
 export async function load({ params }) {
+    //@ts-ignore
+    global.EventSource = eventsource
     const post = await pb
         .collection('posts')
-        .getOne(params.id, { expand: 'user' })
-        .catch((err) => {
-            throw error(404, 'Not found')
-        })
+        .getOne(params.id, { expand: 'user', $autoCancel: false })
 
     if (post) return { post: JSON.parse(JSON.stringify(post)) }
 
