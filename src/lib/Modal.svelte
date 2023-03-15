0512f18d58c5e5184cd14e3fc4d115b65b63a4ab
Xylight <james@xylight.us>
2023-03-15 12:36:57 -0700

    style: Increase contrast

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -14,7 +14,7 @@
     on:close={() => (open = false)}
     on:click|self={() => dialog.close()}
     class={`min-h-[30vh] min-w-[30vw] open:flex flex-col backdrop:bg-black backdrop:bg-opacity-30 open:backdrop:animate-fade-in align-middle rounded-lg
-     shadow-lg bg-slate-50 dark:bg-slate-800 text-inherit scalein`}
+     shadow-lg bg-white dark:bg-slate-800 text-inherit scalein`}
 >
     <button
         class="inline-flex relative top-0 left-0 flex-row gap-1 items-center w-min opacity-50 transition-opacity hover:opacity-75 focus:border-none"
