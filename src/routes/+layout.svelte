4cfdc61d265206c78806d0870391dc245494b0da
Xylight <james@xylight.us>
2023-03-21 09:34:05 -0700

    design: Make gradient blue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,25 +42,26 @@
             class="relative box-border w-[64rem] h-auto rotate-[30deg] overflow-hidden -top-96 -left-96 blur-3xl"
             viewBox="0 0 1155 678"
             xmlns="http://www.w3.org/2000/svg"
-            ><path
+        >
+            <path
                 d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
                 fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
                 fill-opacity=".3"
-            /><defs
-                ><linearGradient
+            />
+            <defs>
+                <linearGradient
                     gradientUnits="userSpaceOnUse"
                     id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
                     x1="1155.49"
                     x2="-78.208"
                     y1=".177"
                     y2="474.645"
-                    ><stop stop-color="#9089FC" /><stop
-                        stop-color="#FF80B5"
-                        offset="1"
-                    /></linearGradient
-                ></defs
-            ></svg
-        >
+                >
+                    <stop stop-color="#72efdd" />
+                    <stop stop-color="#4d9aff" offset="1" />
+                </linearGradient>
+            </defs>
+        </svg>
     </div>
 </div>
 
