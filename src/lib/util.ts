eaa5e3611a43cfdcd2c51b3f4deceb22c5b13352
Xylight <james@xylight.us>
2023-03-22 12:55:06 -0700

    fix: Fix isVideo()

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,7 +1,7 @@
 export const isVideo = (url: string) => {
     const types = ['mp4', 'mkv', 'webm', 'mov']
 
-    for (const type in types) {
+    for (const type of types) {
         if (new URL(url).pathname.toLowerCase().endsWith(type)) return true
     }
 
