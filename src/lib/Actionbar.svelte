84f82ca301cda11a9cf91b51b0b93e35c3481ca6
Xylight <james@xylight.us>
2023-04-05 12:02:01 -0700

    design: Add translucent actionbar to dark theme

--- src/lib/Actionbar.svelte
+++ src/lib/Actionbar.svelte
@@ -31,7 +31,7 @@
 
 {#if open}
     <div
-        class="fixed top-0 w-full backdrop-blur-2xl bg-white dark:bg-slate-800 bg-opacity-90 h-16 p-4 flex flex-row items-center z-20 m-0 left-0"
+        class="fixed top-0 w-full backdrop-blur-2xl bg-white dark:bg-slate-800 bg-opacity-90 dark:bg-opacity-80 h-16 p-4 flex flex-row items-center z-20 m-0 left-0"
         transition:popIn={{ duration: 750 }}
     >
         <div class="mr-auto rounded-full w-12 h-12">
