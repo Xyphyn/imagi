aad8c1fd1fad13cd1ac0ce07e0392be071c587eb
Xyphyn <james@xylight.dev>
2023-08-09 14:01:11 -0700

    feat: add download button

--- src/lib/misc/posts/PostView.svelte
+++ src/lib/misc/posts/PostView.svelte
@@ -1,141 +1,137 @@
 <script lang="ts">
-    import { goto } from '$app/navigation'
-    import { pb, user } from '$lib/backend/pocketbase'
-    import {
-        Collections,
-        type CommunitiesResponse,
-        type PostCountsResponse,
-        type PostsResponse,
-        type UsersResponse,
-    } from '$lib/backend/schema'
-    import AdvancedModal from '$lib/ui/AdvancedModal.svelte'
-    import Button from '$lib/ui/Button.svelte'
-    import Menu from '$lib/ui/menus/Menu.svelte'
-    import {
-        ChatBubbleLeftEllipsis,
-        ChatBubbleOvalLeftEllipsis,
-        EllipsisHorizontal,
-        Icon,
-        Square2Stack,
-        Trash,
-    } from 'svelte-hero-icons'
-    import Comments from '../comments/Comments.svelte'
-    import { isVideo } from './util'
-    import LikeButton from '../likes/LikeButton.svelte'
-    import { Color } from '$lib/ui/colors'
-    import MenuButton from '$lib/ui/menus/MenuButton.svelte'
-    import { page } from '$app/stores'
-    import RelativeDate from '../RelativeDate.svelte'
-    import { _ } from 'svelte-i18n'
-    import Avatar from '$lib/ui/profile/Avatar.svelte'
+  import { goto } from '$app/navigation'
+  import { pb, user } from '$lib/backend/pocketbase'
+  import {
+    Collections,
+    type CommunitiesResponse,
+    type PostCountsResponse,
+    type PostsResponse,
+    type UsersResponse,
+  } from '$lib/backend/schema'
+  import AdvancedModal from '$lib/ui/AdvancedModal.svelte'
+  import Button from '$lib/ui/Button.svelte'
+  import Menu from '$lib/ui/menus/Menu.svelte'
+  import {
+    ChatBubbleLeftEllipsis,
+    ChatBubbleOvalLeftEllipsis,
+    EllipsisHorizontal,
+    Icon,
+    Square2Stack,
+    Trash,
+    ArrowDownTray,
+  } from 'svelte-hero-icons'
+  import Comments from '../comments/Comments.svelte'
+  import { isVideo } from './util'
+  import LikeButton from '../likes/LikeButton.svelte'
+  import { Color } from '$lib/ui/colors'
+  import MenuButton from '$lib/ui/menus/MenuButton.svelte'
+  import { page } from '$app/stores'
+  import RelativeDate from '../RelativeDate.svelte'
+  import { _ } from 'svelte-i18n'
+  import Avatar from '$lib/ui/profile/Avatar.svelte'
 
-    export let open = false
-    export let post:
-        | PostsResponse<{
-              user: UsersResponse
-              community: CommunitiesResponse
-              'postCounts(post)': PostCountsResponse[]
-          }>
-        | undefined = undefined
+  export let open = false
+  export let post:
+    | PostsResponse<{
+        user: UsersResponse
+        community: CommunitiesResponse
+        'postCounts(post)': PostCountsResponse[]
+      }>
+    | undefined = undefined
 
-    let deleting = false
+  let deleting = false
 </script>
 
 {#if post}
-    <AdvancedModal bind:open>
-        <div class="flex flex-col" slot="title">
-            <h1 class="text-xl font-bold">{post?.description}</h1>
-            <span
-                class="flex flex-row gap-1 items-center mt-1 text-base opacity-80"
-            >
-                <button
-                    on:click={() =>
-                        goto(`/user/${post?.expand?.user.username}`)}
-                    class="flex flex-row gap-2 items-center transition-colors hover:text-sky-500"
-                >
-                    <Avatar
-                        width={20}
-                        thumb="32x32"
-                        user={post?.expand?.user}
-                    />
-                    {post?.expand?.user.username}
-                </button>
+  <AdvancedModal bind:open>
+    <div class="flex flex-col" slot="title">
+      <h1 class="text-xl font-bold">{post?.description}</h1>
+      <span class="flex flex-row gap-1 items-center mt-1 text-base opacity-80">
+        <button
+          on:click={() => goto(`/user/${post?.expand?.user.username}`)}
+          class="flex flex-row gap-2 items-center transition-colors hover:text-sky-500"
+        >
+          <Avatar width={20} thumb="32x32" user={post?.expand?.user} />
+          {post?.expand?.user.username}
+        </button>
 
-                {#if post?.expand?.community}
-                    •
-                    <button
-                        on:click={() =>
-                            goto(`/community/${post?.expand?.community.name}`)}
-                        class="transition-colors hover:text-sky-500"
-                    >
-                        {post?.expand?.community.name}
-                    </button>
-                {/if}
-            </span>
+        {#if post?.expand?.community}
+          •
+          <button
+            on:click={() => goto(`/community/${post?.expand?.community.name}`)}
+            class="transition-colors hover:text-sky-500"
+          >
+            {post?.expand?.community.name}
+          </button>
+        {/if}
+      </span>
+    </div>
+    <div class="flex flex-col gap-4 items-center mt-4 w-full">
+      {#if isVideo(pb.getFileUrl(post, post?.image))}
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video controls loop class="max-w-xl max-h-[80vh] rounded-lg w-full">
+          <source src={pb.getFileUrl(post, post?.image)} />
+        </video>
+      {:else}
+        <img
+          src={pb.getFileUrl(post, post?.image)}
+          alt={post.alt_text || post.description}
+          class="w-full max-w-xl rounded-lg"
+          width={400}
+          height={200}
+        />
+      {/if}
+      {#if post?.alt_text}
+        <div
+          class="p-2 px-4 w-full max-w-full text-sm break-words rounded-md bg-black/5 dark:bg-white/5"
+        >
+          {post?.alt_text}
         </div>
-        <div class="flex flex-col gap-4 items-center mt-4 w-full">
-            {#if isVideo(pb.getFileUrl(post, post?.image))}
-                <!-- svelte-ignore a11y-media-has-caption -->
-                <video
-                    controls
-                    loop
-                    class="max-w-xl max-h-[80vh] rounded-lg w-full"
-                >
-                    <source src={pb.getFileUrl(post, post?.image)} />
-                </video>
-            {:else}
-                <img
-                    src={pb.getFileUrl(post, post?.image)}
-                    alt={post.alt_text || post.description}
-                    class="w-full max-w-xl rounded-lg"
-                    width={400}
-                    height={200}
-                />
-            {/if}
-            {#if post?.alt_text}
-                <div
-                    class="p-2 px-4 w-full max-w-full text-sm break-words rounded-md bg-black/5 dark:bg-white/5"
-                >
-                    {post?.alt_text}
-                </div>
-            {/if}
-            <div class="flex flex-row gap-2 items-center w-full">
-                <div class="self-start mr-auto text-sm opacity-50">
-                    <RelativeDate date={post.created} />
-                </div>
-                <LikeButton {post} />
-                <Menu itemsClass="bg-slate-100 dark:bg-zinc-800">
-                    <Button color={Color.ghost} slot="button" class="h-9">
-                        <Icon src={EllipsisHorizontal} size="18" mini />
-                    </Button>
-                    <MenuButton
-                        onclick={() =>
-                            navigator.clipboard.writeText(
-                                `${$page.url.protocol}//${$page.url.hostname}/post/${post?.id}`
-                            )}
-                    >
-                        <Icon src={Square2Stack} size="18" />
-                        {$_('button.post.copy')}
-                    </MenuButton>
-                    {#if post && post.user == $user?.id}
-                        <MenuButton
-                            onclick={async () => {
-                                if (!post) return
-                                deleting = true
-                                await pb
-                                    .collection(Collections.Posts)
-                                    .delete(post.id)
-                                deleting = false
-                            }}
-                            color={Color.dangerSecondary}
-                        >
-                            <Icon src={Trash} mini size="18" />
-                            {$_('button.action.delete')}
-                        </MenuButton>
-                    {/if}
-                </Menu>
-            </div>
-            <Comments {post} />
+      {/if}
+      <div class="flex flex-row gap-2 items-center w-full">
+        <div class="self-start mr-auto text-sm opacity-50">
+          <RelativeDate date={post.created} />
         </div>
-    </AdvancedModal>
+        <LikeButton {post} />
+        <Menu itemsClass="bg-slate-100 dark:bg-zinc-800">
+          <Button color={Color.ghost} slot="button" class="h-9">
+            <Icon src={EllipsisHorizontal} size="18" mini />
+          </Button>
+          <MenuButton
+            onclick={() =>
+              navigator.clipboard.writeText(
+                `${$page.url.protocol}//${$page.url.hostname}/post/${post?.id}`
+              )}
+          >
+            <Icon src={Square2Stack} size="18" />
+            {$_('button.post.copy')}
+          </MenuButton>
+          <MenuButton
+            onclick={() =>
+              navigator.clipboard.writeText(
+                `${pb.getFileUrl(post, post?.image)}`
+              )}
+          >
+            <Icon src={ArrowDownTray} size="18" />
+            Download
+          </MenuButton>
+          {#if post && post.user == $user?.id}
+            <MenuButton
+              onclick={async () => {
+                if (!post) return
+                deleting = true
+                await pb.collection(Collections.Posts).delete(post.id)
+                deleting = false
+              }}
+              color={Color.dangerSecondary}
+            >
+              <Icon src={Trash} mini size="18" />
+              {$_('button.action.delete')}
+            </MenuButton>
+          {/if}
+        </Menu>
+      </div>
+      <Comments {post} />
+    </div>
+  </AdvancedModal>
 {/if}
--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -1,120 +1,125 @@
 <script lang="ts">
-    import { pb, user } from '$lib/backend/pocketbase.js'
-    import Comments from '$lib/misc/comments/Comments.svelte'
-    import LikeButton from '$lib/misc/likes/LikeButton.svelte'
-    import { isVideo } from '$lib/misc/posts/util.js'
-    import Button from '$lib/ui/Button.svelte'
-    import { Color } from '$lib/ui/colors.js'
-    import Menu from '$lib/ui/menus/Menu.svelte'
-    import MenuButton from '$lib/ui/menus/MenuButton.svelte'
-    import {
-        ChatBubbleOvalLeftEllipsis,
-        EllipsisHorizontal,
-        Icon,
-        Square2Stack,
-        Trash,
-    } from 'svelte-hero-icons'
-    import { page } from '$app/stores'
-    import { Collections } from '$lib/backend/schema.js'
-    import { _ } from 'svelte-i18n'
+  import { pb, user } from '$lib/backend/pocketbase.js'
+  import Comments from '$lib/misc/comments/Comments.svelte'
+  import LikeButton from '$lib/misc/likes/LikeButton.svelte'
+  import { isVideo } from '$lib/misc/posts/util.js'
+  import Button from '$lib/ui/Button.svelte'
+  import { Color } from '$lib/ui/colors.js'
+  import Menu from '$lib/ui/menus/Menu.svelte'
+  import MenuButton from '$lib/ui/menus/MenuButton.svelte'
+  import {
+    ChatBubbleOvalLeftEllipsis,
+    EllipsisHorizontal,
+    Icon,
+    Square2Stack,
+    Trash,
+    ArrowDownTray,
+  } from 'svelte-hero-icons'
+  import { page } from '$app/stores'
+  import { Collections } from '$lib/backend/schema.js'
+  import { _ } from 'svelte-i18n'
 
-    export let data
+  export let data
 </script>
 
 <title>{data.post.description}</title>
 <meta property="og:title" content={data.post.description} />
 <meta
-    property="og:description"
-    content={data.post.alt_text ||
-        'View this post on Imagi, a real time social app.'}
+  property="og:description"
+  content={data.post.alt_text ||
+    'View this post on Imagi, a real time social app.'}
 />
 {#if isVideo(pb.getFileUrl(data.post, data.post.image))}
-    <meta
-        property="og:video"
-        content={pb.getFileUrl(data.post, data.post.image)}
-    />
+  <meta
+    property="og:video"
+    content={pb.getFileUrl(data.post, data.post.image)}
+  />
 {/if}
 <meta property="og:image" content={pb.getFileUrl(data.post, data.post.image)} />
 <meta property="#fff" data-react-helmet="true" name="theme-color" />
 <meta name="twitter:card" content="summary_large_image" />
 
 <div
-    class="flex flex-col gap-4 p-6 mx-auto w-max max-w-full bg-white rounded-lg shadow-lg dark:bg-zinc-900"
+  class="flex flex-col gap-4 p-6 mx-auto w-max max-w-full bg-white rounded-lg shadow-lg dark:bg-zinc-900"
 >
-    <div class="flex flex-col">
-        <h1 class="text-xl font-bold">{data.post?.description}</h1>
-        <span class="text-base opacity-80">
-            <a
-                href={`/user/${data.post?.expand?.user.username}`}
-                class="transition-colors hover:text-sky-500"
-            >
-                {data.post?.expand?.user.username}
-            </a>
+  <div class="flex flex-col">
+    <h1 class="text-xl font-bold">{data.post?.description}</h1>
+    <span class="text-base opacity-80">
+      <a
+        href={`/user/${data.post?.expand?.user.username}`}
+        class="transition-colors hover:text-sky-500"
+      >
+        {data.post?.expand?.user.username}
+      </a>
 
-            {#if data.post?.expand?.community}
-                <a
-                    href={`/community/${data.post?.expand?.community.name}`}
-                    class="transition-colors hover:text-sky-500"
-                >
-                    • {data.post?.expand?.community.name}
-                </a>
-            {/if}
-        </span>
-    </div>
-    <div class="flex flex-col gap-4 items-center mt-4 w-full">
-        {#if isVideo(pb.getFileUrl(data.post, data.post?.image))}
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video
-                controls
-                loop
-                class="max-w-xl max-h-[80vh] rounded-lg w-full"
-            >
-                <source src={pb.getFileUrl(data.post, data.post?.image)} />
-            </video>
-        {:else}
-            <img
-                src={pb.getFileUrl(data.post, data.post?.image)}
-                alt={data.post.alt_text || data.post.description}
-                class="w-full max-w-xl rounded-lg"
-                width={400}
-                height={200}
-            />
+      {#if data.post?.expand?.community}
+        <a
+          href={`/community/${data.post?.expand?.community.name}`}
+          class="transition-colors hover:text-sky-500"
+        >
+          • {data.post?.expand?.community.name}
+        </a>
+      {/if}
+    </span>
+  </div>
+  <div class="flex flex-col gap-4 items-center mt-4 w-full">
+    {#if isVideo(pb.getFileUrl(data.post, data.post?.image))}
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video controls loop class="max-w-xl max-h-[80vh] rounded-lg w-full">
+        <source src={pb.getFileUrl(data.post, data.post?.image)} />
+      </video>
+    {:else}
+      <img
+        src={pb.getFileUrl(data.post, data.post?.image)}
+        alt={data.post.alt_text || data.post.description}
+        class="w-full max-w-xl rounded-lg"
+        width={400}
+        height={200}
+      />
+    {/if}
+    <div class="flex flex-row gap-2 ml-auto">
+      <div class="flex flex-row gap-1 items-center">
+        <Icon src={ChatBubbleOvalLeftEllipsis} size="18" mini />
+        {data.post.expand?.['postCounts(post)'][0].comments}
+      </div>
+      <LikeButton post={data.post} />
+      <Menu>
+        <Button color={Color.ghost} slot="button" class="h-9">
+          <Icon src={EllipsisHorizontal} size="18" mini />
+        </Button>
+        <MenuButton
+          onclick={() =>
+            navigator.clipboard.writeText(
+              `${$page.url.protocol}//${$page.url.hostname}/post/${data.post?.id}`
+            )}
+        >
+          <Icon src={Square2Stack} size="18" />
+          {$_('button.post.copy')}
+        </MenuButton>
+
+        <MenuButton
+          onclick={() =>
+            navigator.clipboard.writeText(
+              `${pb.getFileUrl(data.post, data.post?.image)}`
+            )}
+        >
+          <Icon src={ArrowDownTray} size="18" />
+          Download
+        </MenuButton>
+        {#if data.post && data.post.user == $user?.id}
+          <MenuButton
+            onclick={async () => {
+              if (!data.post) return
+              await pb.collection(Collections.Posts).delete(data.post.id)
+            }}
+            color={Color.dangerSecondary}
+          >
+            <Icon src={Trash} mini size="18" />
+            {$_('button.action.delete')}
+          </MenuButton>
         {/if}
-        <div class="flex flex-row gap-2 ml-auto">
-            <div class="flex flex-row gap-1 items-center">
-                <Icon src={ChatBubbleOvalLeftEllipsis} size="18" mini />
-                {data.post.expand?.['postCounts(post)'][0].comments}
-            </div>
-            <LikeButton post={data.post} />
-            <Menu>
-                <Button color={Color.ghost} slot="button" class="h-9">
-                    <Icon src={EllipsisHorizontal} size="18" mini />
-                </Button>
-                <MenuButton
-                    onclick={() =>
-                        navigator.clipboard.writeText(
-                            `${$page.url.protocol}//${$page.url.hostname}/post/${data.post?.id}`
-                        )}
-                >
-                    <Icon src={Square2Stack} size="18" />
-                    {$_('button.post.copy')}
-                </MenuButton>
-                {#if data.post && data.post.user == $user?.id}
-                    <MenuButton
-                        onclick={async () => {
-                            if (!data.post) return
-                            await pb
-                                .collection(Collections.Posts)
-                                .delete(data.post.id)
-                        }}
-                        color={Color.dangerSecondary}
-                    >
-                        <Icon src={Trash} mini size="18" />
-                        {$_('button.action.delete')}
-                    </MenuButton>
-                {/if}
-            </Menu>
-        </div>
-        <Comments post={data.post} />
+      </Menu>
     </div>
+    <Comments post={data.post} />
+  </div>
 </div>
